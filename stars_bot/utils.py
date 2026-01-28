b0103b4099c352bd4c12db6c4866a557305dcc92
krevetochka37 <131526183+krevetochka37@users.noreply.github.com>
2026-01-28 15:11:47 +0300

    Update utils.py

--- stars_bot/utils.py
+++ stars_bot/utils.py
@@ -5,21 +5,7 @@ import math
 def get_stars_amount_for_credits(credits: int) -> int:
     """
     Рассчитывает количество звезд Telegram для указанного количества кредитов
-    Использует точные значения из пресетов:
-    - 150 credits = 400 stars ($6)
-    - 250 credits = 650 stars ($10)
-    - 500 credits = 1300 stars ($20)
-    - 1000 credits = 2600 stars ($40)
-    - 1500 credits = 3850 stars ($60)
-    - 2000 credits = 5150 stars ($80)
-    
-    Args:
-        credits: Количество кредитов
-        
-    Returns:
-        Количество звезд Telegram
     """
-    # Точные значения из пресетов
     stars_map = {
         150: 400,
         250: 650,
@@ -29,7 +15,6 @@ def get_stars_amount_for_credits(credits: int) -> int:
         2000: 5150
     }
     
-    # Если это точное значение из пресета - возвращаем его
     if credits in stars_map:
         return stars_map[credits]
     
@@ -39,14 +24,10 @@ def get_stars_amount_for_credits(credits: int) -> int:
     
     # Если запрошено меньше минимального пресета
     if credits < sorted_presets[0]:
-        # Используем курс минимального пресета: 150 credits = 400 stars
-        # 1 credit = 400/150 = 2.67 stars
         return int(credits * 400 / 150)
     
     # Если запрошено больше максимального пресета
     if credits > sorted_presets[-1]:
-        # Используем курс максимального пресета: 2000 credits = 5150 stars
-        # 1 credit = 5150/2000 = 2.575 stars
         return int(credits * 5150 / 2000)
     
     # Находим два ближайших пресета для интерполяции
